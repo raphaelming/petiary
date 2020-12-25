f5a6980eb3e2ec266570fe0165431660c319db5d
raphaelming <spear131121@gmail.com>
2020-12-26 00:56:36 +0900

    init

---
+++ petiary_ios/petiary/petiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  petiaryApp.swift
+//  petiary
+//
+//  Created by 이민혁 on 2020/12/26.
+//
+
+import SwiftUI
+
+@main
+struct petiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
